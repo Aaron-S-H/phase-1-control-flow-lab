08a07623388deb92e2acb8393ccaa4ab31e4df24
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-04-26 15:20:04 +0200

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let greeting;
+  if (distance <= 400 ) {
+    return greeting = 'This one is on me!';
+  } else if 
+    (distance >= 400 && distance <= 2000) {
+     return greeting = 'That will be twenty bucks.';
+  } else if 
+    (distance > 2000 && distance < 2500) {
+     return greeting = 'I will gladly take your thirty bucks.';
+    } else {
+      return greeting = 'No can do.';
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city) {
+return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip){ 
+    case 'generous':
+      return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default: 
+      return 'Bye.';
+
+ 
+}
 }
