a63514d0d196bdc8601fe82c0e51eb2c65def63e
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-04-26 11:09:48 +0200

    commit tree aeebaeb4106e26e008c42a09858993f8de2fa549

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  let greeting;
+  if (distance <= 400 ) {
+    return greeting = 'This one is on me!';
+  } else if 
+    (distance >= 400 && distance <= 2000) {
+     return greeting = 'That will be twenty bucks.';
+  } else if 
+    (distance > 2000 && distance < 2500) {
+     return greeting = 'I will gladly take your thirty bucks.';
+    } else {
+      return greeting = 'No can do.';
+    }
 }
+
+
 
 function ternaryCheckCity(){
   // Write your code here!
